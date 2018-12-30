23e17e16b9b308f4d6c884276ae04b113fdb2ef5
tdude92 <41596427+tdude92@users.noreply.github.com>
2018-12-30 15:44:07 -0500

    Update game.py
    - Made it easier to import this file's functions into other files.
    - Added a way to exit the game from the console.

--- game.py
+++ game.py
@@ -1,17 +1,4 @@
-from copy import deepcopy
-from time import sleep
-
-gridLength = int(input("Grid Length: "))
-gridWidth = int(input("Grid Width: "))
-
-with open("seed.txt", "r") as seed:
-    seedCells = seed.readlines()
-    for i in range(len(seedCells)):
-        seedCells[i] = list(map(int, seedCells[i][:len(seedCells[i]) - 1].split()))
-
-grid = [["0" if [x, y] in seedCells else "." for x in range(gridLength)] for y in range(gridWidth)]
-
-def neighbors(x, y):
+def neighbors(x, y, grid, gridLength, gridWidth):
     out = 0
     if (x, y) == (0, 0):
         if grid[y][x + 1] == "0":
@@ -104,38 +91,54 @@ def neighbors(x, y):
             out += 1
     return out
 
-def isAlive(x, y):
+def isAlive(x, y, grid, gridLength, gridWidth):
     if grid[y][x] == "0":
         alreadyAlive = True
     else:
         alreadyAlive = False
 
-    if neighbors(x, y) < 2:
+    if neighbors(x, y, grid, gridLength, gridWidth) < 2:
         return "."
         # Any cell with < 2 neighbors dies.
-    elif neighbors(x, y) in (2, 3) and alreadyAlive:
+    elif neighbors(x, y, grid, gridLength, gridWidth) in (2, 3) and alreadyAlive:
         return "0"
         # Any living cell with 2 or 3 neighbors lives.
-    elif neighbors(x, y) > 3:
+    elif neighbors(x, y, grid, gridLength, gridWidth) > 3:
         return "."
         # Any cell with more than 3 neighbors dies.
-    elif neighbors(x, y) == 3 and not alreadyAlive:
+    elif neighbors(x, y, grid, gridLength, gridWidth) == 3 and not alreadyAlive:
         return "0"
         # Any dead cell with 3 neighbors lives.
     else:
         return "."
 
-while True:
-    for i in grid:
-        print(*i) 
-    print()
+if __name__ == "__main__":
+    from copy import deepcopy
+    from time import sleep
 
-    gridCopy = deepcopy(grid)
-    
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            gridCopy[y][x] = isAlive(x, y) 
-    
-    grid = deepcopy(gridCopy)
-    
-    sleep(0.5)
+    gridLength = int(input("Grid Length: "))
+    gridWidth = int(input("Grid Width: "))
+
+    with open("seed.txt", "r") as seed:
+        seedCells = seed.readlines()
+        for i in range(len(seedCells)):
+            seedCells[i] = list(map(int, seedCells[i][:len(seedCells[i]) - 1].split()))
+
+    grid = [["0" if [x, y] in seedCells else "." for x in range(gridLength)] for y in range(gridWidth)]
+
+    while True:
+        for i in grid:
+            print(*i) 
+        print()
+
+        gridCopy = deepcopy(grid)
+        for y in range(len(grid)):
+            for x in range(len(grid[y])):
+                gridCopy[y][x] = isAlive(x, y, grid, gridLength, gridWidth) 
+        
+        grid = deepcopy(gridCopy)
+
+        n = input("Press <Enter> to continue, input 'n' to stop.\n")
+        
+        if n == "n":
+            break
